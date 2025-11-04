d8d33ff4ce35280b9e40dbbfa7577f60314bae72
bscx <bscx@example.com>
2025-11-04 17:25:34 +0700

    update flake lock

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
     };
   };
 
-  outputs = { self, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, homebrew-kpt, homebrew-doppler, homebrew-flux, homebrew-fuse, homebrew-youtube-music, homebrew-zadark, home-manager, nixpkgs, nixpkgs-specific, disko, nix-search-cli } @inputs:
+  outputs = { self, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, homebrew-kpt, homebrew-doppler, homebrew-fuse, homebrew-youtube-music, homebrew-zadark, home-manager, nixpkgs, nixpkgs-specific, disko, nix-search-cli } @inputs:
     let
       user = "%USER%";
       linuxSystems = [ "x86_64-linux" "aarch64-linux" ];
